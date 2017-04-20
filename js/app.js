dbfc6815ebff0a16517955de5ceacab4e45baa90
nancyzj <ancizj@hotmail.com>
2017-04-20 15:12:36 -0500

    fotos ordenadas

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 var hoja = document.getElementsByTagName("BODY")[0];
 var foto;
 var contador = 0;
-// var imagenCoder = document.getElementsByClassName('imagenes');
-// var longitud = imagenCoder.length;
-// console.log(imagenCoder);
 //crear elementos
 var section = document.createElement('section');
 //dar propiedades a los elementos
 //section
-section.style.width = "600px";
-section.style.height = "500px";
+section.style.width = "1000px";
+section.style.height = "700px";
+section.style.left = "200px";
+section.style.top = "100px";
+section.style.position = "relative";
 section.style.border = "thick solid #0000FF"; //le puedes poner un 1px y si lo lee
 //fin de la section
 //crear una funcion para las imagenes
@@ -22,13 +22,13 @@ var fotos = function(){
 	console.log(ruta); 
 	foto1.src = ruta;
 	foto1.className = "imagenes";
-	foto1.style.width = "100px";
-	foto1.style.height = "100px";
+	foto1.style.width = "200px";
+	foto1.style.height = "200px";
 	foto1.style.position = "relative";
 	return foto1;
 }
 //funcion de los div 
-var cuadros = function(color,ancho,altura,posicion,alineamiento,valorTop,valorRight,valorLeft,valorBottom,marginLeft){
+var cuadros = function(color,ancho,altura,posicion,alineamiento,valorTop,valorBottom,marginLeft){
 	var cuadro = document.createElement('div');
 	cuadro.style.border = color;
 	cuadro.style.width = ancho;
@@ -36,19 +36,17 @@ var cuadros = function(color,ancho,altura,posicion,alineamiento,valorTop,valorRi
 	cuadro.style.position = posicion;
 	cuadro.style.display = alineamiento;
 	cuadro.style.top = valorTop;
-	cuadro.style.right = valorRight;
-	cuadro.style.left = valorLeft;
 	cuadro.style.bootom = valorBottom;
 	cuadro.style.marginLeft = marginLeft;
 	cuadro.appendChild(fotos());	 
 	section.appendChild(cuadro);
-}
-cuadros('10px solid red','200px','100px','relative','inline-block','100px','0','0','50px','80px');	
-cuadros('10px solid green','200px','100px','relative','inline-block','100px','0px','150','50px','80px');	
-cuadros('10px solid purple','100px','100px','relative','inline-block','100px','0px','250','50px');	
-cuadros('10px solid yellow','100px','100px','relative','inline-block','100px','0px','300px','50px');	
-cuadros('10px solid grey','100px','100px','relative','inline-block','100px','0px','450','50px');	
-cuadros('10px solid black','100px','100px','relative','inline-block','100px','0px','550','50px');	
+}																//top     bottom margin
+cuadros('10px solid red','200px','200px','relative','inline-block','100px','50px','80px');	
+cuadros('10px solid green','200px','200px','relative','inline-block','100px','50px','80px');	
+cuadros('10px solid purple','200px','200px','relative','inline-block','100px','50px','80px');	
+cuadros('10px solid yellow','200px','200px','relative','inline-block','200px','50px','80px');	
+cuadros('10px solid grey','200px','200px','relative','inline-block','200px','50px','80px');	
+cuadros('10px solid black','200px','200px','relative','inline-block','200px','50px','80px');	
 //dibujar
 hoja.appendChild(section);
 
